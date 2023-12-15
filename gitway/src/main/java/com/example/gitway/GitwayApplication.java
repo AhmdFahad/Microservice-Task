0ded30718127e2b51df32819046e4156f16e95eb
AhmdFahad <ahmd66699@gmail.com>
2023-12-15 17:07:56 +0300

    Create Api getway

---
+++ gitway/src/main/java/com/example/gitway/GitwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitwayApplication.class, args);
+    }
+
+}
