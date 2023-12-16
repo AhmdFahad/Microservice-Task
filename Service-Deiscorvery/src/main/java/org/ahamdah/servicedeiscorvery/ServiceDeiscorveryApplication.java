9e6f469c49407ceca07dc93a70a1b9d18188683a
AhmdFahad <ahmd66699@gmail.com>
2023-12-16 19:45:50 +0300

    add service discovery

---
+++ Service-Deiscorvery/src/main/java/org/ahamdah/servicedeiscorvery/ServiceDeiscorveryApplication.java
@@ -0,0 +1,15 @@
+package org.ahamdah.servicedeiscorvery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDeiscorveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeiscorveryApplication.class, args);
+    }
+
+}
---
+++ Service-Deiscorvery/src/test/java/org/ahamdah/servicedeiscorvery/ServiceDeiscorveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahamdah.servicedeiscorvery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeiscorveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
