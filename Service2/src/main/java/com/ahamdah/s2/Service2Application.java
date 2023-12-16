b1251667254686c48ffd8af5880f0a9cf1aa419d
AhmdFahad <ahmd66699@gmail.com>
2023-12-16 21:07:37 +0300

    add eureka client

--- Service2/src/main/java/com/ahamdah/s2/Service2Application.java
+++ Service2/src/main/java/com/ahamdah/s2/Service2Application.java
@@ -1,23 +1,12 @@
 package com.ahamdah.s2;
 
-import com.ahamdah.s2.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service2Application implements CommandLineRunner {
-
+public class Service2Application  {
     public static void main(String[] args) {
         SpringApplication.run(Service2Application.class, args);
     }
 
-    @Autowired
-    private TransactionRepository transactionRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
--- Service3/src/main/java/com/ahamdah/s2/Service3Application.java
+++ Service3/src/main/java/com/ahamdah/s2/Service3Application.java
@@ -1,20 +1,13 @@
 package com.ahamdah.s2;
 
-import com.ahamdah.s2.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service3Application implements CommandLineRunner {
+public class Service3Application  {
 
     public static void main(String[] args) {
         SpringApplication.run(Service3Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
