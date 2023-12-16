852bfb1d93ce4eb64df71ddc79fcb7caa62edf94
AhmdFahad <ahmd66699@gmail.com>
2023-12-16 19:45:25 +0300

    refactor in service 3

--- Service3/src/main/java/com/ahamdah/s2/Service3Application.java
+++ Service3/src/main/java/com/ahamdah/s2/Service3Application.java
@@ -5,13 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class S2Application implements CommandLineRunner {
+public class Service3Application implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(S2Application.class, args);
+        SpringApplication.run(Service3Application.class, args);
     }
 
     @Autowired
